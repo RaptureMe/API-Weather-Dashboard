33be4559bf27c499e759ff5202e397e67d540204
RaptureMe <arosu4u@gmail.com>
2023-08-03 15:24:10 -0700

    Added bootstrap + some styling to navbar/containers -- Added present day+forecast weather API, linked the text content to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@ var apiKey = "70498a7525a378189dcd476cd02c3e10";
 var searchBtn = document.querySelector(".btn");
 var userInput = document.querySelector(".me-2");
 
-
 function searchWeather(event) {
     event.preventDefault()
     var currentWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${userInput.value}&appid=${apiKey}&units=imperial`;
@@ -14,18 +13,34 @@ function searchWeather(event) {
   })
   .then(function (data) {
     console.log(data);
-    document.querySelector(".userinput").textContent = userInput.value
-    document.querySelector(".current").textContent = data.main.temp
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=70498a7525a378189dcd476cd02c3e10`, {
+    document.querySelector(".userinput").textContent = data.name
+    document.querySelector(".current").textContent = data.main.temp + " degrees today!"
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apiKey}&units=imperial`, {
         method: 'GET',
       })
         .then(function (response) {
           return response.json();
         })
         .then(function (data) {
+          for ( var i = 1; i<=5; i++) {
           console.log(data);
-          document.querySelector("")
+          document.querySelector(".day"+i).innerHTML = data.list[i*8-4].main.temp + "<br>" + data.list[i*8-4].dt_txt.split(' ')[0];
+          }
+        //   /* document.querySelector(".day2").innerHTML = data.list[12].main.temp + "<br>" + data.list[4].dt_txt.split(' ')[0];
+        //   document.querySelector(".day3").innerHTML = data.list[20].main.temp + "<br>" + data.list[4].dt_txt.split(' ')[0];
+        //   document.querySelector(".day4").innerHTML = data.list[28].main.temp + "<br>" + data.list[4].dt_txt.split(' ')[0];
+        //   document.querySelector(".day5").innerHTML = data.list[36].main.temp + "<br>" + data.list[4].dt_txt.split(' ')[0]; */
+        // }
+
+        // var count = 1;
+        // for ( var i = 4; i<=36; i=i+8) {
+        //   console.log(data);
+        //   document.querySelector(".day"+count).innerHTML = data.list[i].main.temp + "<br>" + data.list[i].dt_txt.split(' ')[0];
+        //   count++;
+        // }
+
         });
+
   });
 };
 

ab152b032201e0ff8d4f40aefc44ed0ac5948de3
RaptureMe <arosu4u@gmail.com>
2023-08-07 09:26:53 -0700

    Added forecast api and current day api, search bar for the city

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,56 +2,60 @@ var apiKey = "70498a7525a378189dcd476cd02c3e10";
 var searchBtn = document.querySelector(".btn");
 var userInput = document.querySelector(".me-2");
 
-function searchWeather(event) {
-    event.preventDefault()
-    var currentWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${userInput.value}&appid=${apiKey}&units=imperial`;
-    fetch(currentWeatherAPI, {
-  method: 'GET',
-})
-  .then(function (response) {
-    return response.json();
+function searchButtonClk(event) {
+  event.preventDefault(); // Was having issues with page reloading, added Prevent
+  searchWeather(userInput.value); // Current day weather API, Adds user Input and API key on search
+}
+
+function forecastWeather(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`,
+    {
+      method: "GET",
+    }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (var i = 1; i <= 5; i++) {
+        console.log(data);
+        document.querySelector(".day" + i).innerHTML =
+          data.list[i * 8 - 4].main.temp +
+          "<br>" +
+          data.list[i * 8 - 4].dt_txt.split(" ")[0];
+      }
+      //   /* document.querySelector(".day2").innerHTML = data.list[12].main.temp + "<br>" + data.list[4].dt_txt.split(' ')[0];
+      //   document.querySelector(".day3").innerHTML = data.list[20].main.temp + "<br>" + data.list[4].dt_txt.split(' ')[0];
+      //   document.querySelector(".day4").innerHTML = data.list[28].main.temp + "<br>" + data.list[4].dt_txt.split(' ')[0];
+      //   document.querySelector(".day5").innerHTML = data.list[36].main.temp + "<br>" + data.list[4].dt_txt.split(' ')[0]; */
+      // }
+
+      // var count = 1;
+      // for ( var i = 4; i<=36; i=i+8) {
+      //   console.log(data);
+      //   document.querySelector(".day"+count).innerHTML = data.list[i].main.temp + "<br>" + data.list[i].dt_txt.split(' ')[0];
+      //   count++;
+      // }
+    });
+}
+
+function searchWeather(city) {
+  var currentWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  fetch(currentWeatherAPI, {
+    method: "GET",
   })
-  .then(function (data) {
-    console.log(data);
-    document.querySelector(".userinput").textContent = data.name
-    document.querySelector(".current").textContent = data.main.temp + " degrees today!"
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apiKey}&units=imperial`, {
-        method: 'GET',
-      })
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          for ( var i = 1; i<=5; i++) {
-          console.log(data);
-          document.querySelector(".day"+i).innerHTML = data.list[i*8-4].main.temp + "<br>" + data.list[i*8-4].dt_txt.split(' ')[0];
-          }
-        //   /* document.querySelector(".day2").innerHTML = data.list[12].main.temp + "<br>" + data.list[4].dt_txt.split(' ')[0];
-        //   document.querySelector(".day3").innerHTML = data.list[20].main.temp + "<br>" + data.list[4].dt_txt.split(' ')[0];
-        //   document.querySelector(".day4").innerHTML = data.list[28].main.temp + "<br>" + data.list[4].dt_txt.split(' ')[0];
-        //   document.querySelector(".day5").innerHTML = data.list[36].main.temp + "<br>" + data.list[4].dt_txt.split(' ')[0]; */
-        // }
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      document.querySelector(".userinput").textContent = data.name;
+      document.querySelector(".current").textContent =
+        data.main.temp + " degrees today!";
+      forecastWeather(data.coord.lat, data.coord.lon);
+    });
+}
 
-        // var count = 1;
-        // for ( var i = 4; i<=36; i=i+8) {
-        //   console.log(data);
-        //   document.querySelector(".day"+count).innerHTML = data.list[i].main.temp + "<br>" + data.list[i].dt_txt.split(' ')[0];
-        //   count++;
-        // }
-
-        });
-
-  });
-};
-
-
-
-
-
-
-
-
-
-
-
-searchBtn.addEventListener("click", searchWeather)
+searchWeather("Orange");
+searchBtn.addEventListener("click", searchButtonClk);
